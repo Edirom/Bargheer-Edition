1beaea402378ae318097e3902d4fa46aaf84d586
Daniel Jettka <research@jettka.de>
2026-07-23 13:15:24 +0200

    refined tooltip display

--- js/tooltips.js
+++ js/tooltips.js
@@ -1,8 +1,10 @@
-// add event listener to all elements with attribute data-tipped-options (capture also elements added later to the DOM)
+// add event listener to all elements with class inline-comment (capture also elements added later to the DOM)
 document.addEventListener('mouseover', function(e) {
-    if (e.target.hasAttribute('data-tipped-options')) {
-        // handle mouseover event
-        var options = e.target.getAttribute('data-tipped-options');
+    if (e.target.classList.contains('inline-comment')) {
+        
+        // get attribute data-tipped-options from parent element
+        var parentElement = e.target.parentElement;
+        var options = parentElement.getAttribute('data-tipped-options');
         var referencedElementId = options.match(/inline:\s*'([^']+)'/)[1];
         var referencedElement = document.getElementById(referencedElementId);
         if (referencedElement) {
@@ -14,14 +16,36 @@ document.addEventListener('mouseover', function(e) {
     }
 }, true);
 
+// add event listener to all elements with class inline-comment (capture also elements added later to the DOM)
 document.addEventListener('mouseout', function(e) {
-    if (e.target.hasAttribute('data-tipped-options')) {
-        // handle mouseout event
-        var options = e.target.getAttribute('data-tipped-options');
+    if (e.target.classList.contains('inline-comment')) {
+        var parentElement = e.target.parentElement;
+        var options = parentElement.getAttribute('data-tipped-options');
         var referencedElementId = options.match(/inline:\s*'([^']+)'/)[1];
         var referencedElement = document.getElementById(referencedElementId);
         if (referencedElement) {
-            referencedElement.style.display = 'none';
+            if (!referencedElement.classList.contains('show')) {
+                referencedElement.style.display = 'none';
+            }
+        }
+    }
+}, true);
+
+// add event listener to all elements with class inline-comment (capture also elements added later to the DOM)
+document.addEventListener('click', function(e) {
+    if (e.target.classList.contains('inline-comment')) {
+        var parentElement = e.target.parentElement;
+        var options = parentElement.getAttribute('data-tipped-options');
+        var referencedElementId = options.match(/inline:\s*'([^']+)'/)[1];
+        var referencedElement = document.getElementById(referencedElementId);
+        if (referencedElement) {
+            // toggle display of referenced element depending on its current state
+            if (referencedElement.classList.contains('show')) {
+                referencedElement.classList.remove('show');
+                referencedElement.style.display = 'none';
+            } else {
+                referencedElement.classList.add('show');
+            }
         }
     }
 }, true);
